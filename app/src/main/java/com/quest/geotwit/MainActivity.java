7f9072569f1279d3713121d65ddf0c40f0760d1e
Juan Carlos Estibariz <Juan.Carlos.Estibariz@software.dell.com>
2016-11-14 18:49:55 -0500

    Add view to display tweets

--- app/src/main/java/com/quest/geotwit/MainActivity.java
+++ app/src/main/java/com/quest/geotwit/MainActivity.java
@@ -1,26 +1,39 @@
 package com.quest.geotwit;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity
-        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener {
+
+    private static final float HUE = 202f;
+
+    private TextView tweetView;
+    private boolean tweetVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tweetView = (TextView) findViewById(R.id.tweet);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -53,15 +66,37 @@ public class MainActivity extends FragmentActivity
 
         // Add a marker in T.O. and move the camera
         LatLng to = new LatLng(43.648, -79.369);
-        map.addMarker(new MarkerOptions().position(to).title("T.O.")
-                .icon(BitmapDescriptorFactory.defaultMarker(202f))); // "Twitter" hue
+        map.addMarker(new MarkerOptions().position(to)
+                .icon(BitmapDescriptorFactory.defaultMarker(HUE))) // "Twitter" hue
+                .setTag("This is a tweet!");
+        map.addMarker(new MarkerOptions().position(new LatLng(43.649, -79.368))
+                .icon(BitmapDescriptorFactory.defaultMarker(HUE))) // "Twitter" hue
+                .setTag("This is another tweet!");
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(to, 14f));
+
         map.setOnMarkerClickListener(this);
-        map.getUiSettings().setMapToolbarEnabled(false);
+        map.setOnMapClickListener(this);
+
+        UiSettings uiSettings = map.getUiSettings();
+        uiSettings.setMapToolbarEnabled(false);
+        uiSettings.setZoomControlsEnabled(true);
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        return false;
+        tweetView.setText((String) marker.getTag());
+        if (!tweetVisible) {
+            tweetVisible = true;
+            tweetView.setVisibility(View.VISIBLE);
+        }
+        return true;
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        if (tweetVisible) {
+            tweetVisible = false;
+            tweetView.setVisibility(View.GONE);
+        }
     }
 }
